1c91f76436cc4c72630a1f0b5d16c8fb3bde8ea2
ishpeeedy <anudgesharma@gmail.com>
2026-01-01 23:07:39 +0530

    add auth backend

---
+++ backend/src/routes/auth.ts
@@ -0,0 +1,65 @@
+import express,{Request, Response} from 'express';
+import jwt from 'jsonwebtoken'
+import User from '../models/User.ts'
+
+const router = express.Router();
+
+router.post('/register',async(req:Request,res:Response)=>{
+    try {
+        const {email,password,name} = req.body;
+        const existingUser = await User.findOne({email})
+        if(existingUser){
+            return res.status(400).json({error:'User already exists'})
+        }
+        const user = new User({email,password,name})
+        await user.save();
+        const token = jwt.sign(
+            {userId:user._id , email:user.email},
+            process.env.JWT_SECRET || 'fallback-secret-key',
+            {expiresIn: '7d'}
+        )
+        res.status(201).json({
+            message:'User created successfully',
+            token,
+            user:{
+                id:user._id,
+                email:user.email,
+                name:user.name,
+            },
+        })
+    }catch(error){
+        res.status(500).json({error:'Server error'})
+    }
+})
+
+router.post('/login',async (req:Request , res:Response)=>{
+    try {
+        const {email, password} = req.body;
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(401).json({error:'Invalid credentials'})
+        }
+        const isMatch =await user.comparePassword(password);
+        if(!isMatch){
+            return res.status(401).json({error:'Invalid credentials'})
+        }
+        const token = jwt.sign(
+            {userId:user._id,email:user.email},
+            process.env.JWT_SECRET || 'fallback-secret-key',
+            {expiresIn: '7d'}
+        )
+        res.json({
+            message:'Login successful',
+            token ,
+            user:{
+                id:user._id,
+                email:user.email,
+                name:user.name,
+            },
+        })
+    }catch(error){
+        res.status(500).json({error:'Server error'})
+    }
+})
+
+export default router;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import authRoutes from './routes/auth.ts'
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors({
     credentials:true
 }));
 app.use(express.json());
+app.use('/api/auth',authRoutes)
 
 app.get('/api/health',(req,res)=>{
     res.json({
