305c3b04b1de6596a95266adf9fad6b5c2056b71
ishpeeedy <anudgesharma@gmail.com>
2026-01-01 09:59:18 +0530

    setup mongo , cors

---
+++ backend/src/server.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+const PORT=process.env.PORT || 5000;
+
+app.use(cors({
+    origin:process.env.CLIENT_URL ||'http://localhost:5173',
+    credentials:true
+}));
+app.use(express.json());
+
+app.get('/api/health',(req,res)=>{
+    res.json({
+        status:'ok',
+        message:'PINNED API is running smoothly',
+        timestamp:new Date().toISOString()
+    });
+});
+
+async function run (){
+    try {
+        await mongoose.connect(process.env.DATABASE_URL || '');
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+run();
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`)
+})
